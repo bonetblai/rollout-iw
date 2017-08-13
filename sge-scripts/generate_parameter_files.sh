dc5ac6be4c48ee1dfb0d1bd4168d5f6e1630a002
Blai Bonet <bonetblai@gmail.com>
2017-08-13 11:29:26 +0200

    new parameter files

--- sge-scripts/generate_parameter_files.sh
+++ sge-scripts/generate_parameter_files.sh
@@ -16,8 +16,8 @@ mkdir -p parameters
 #done
 
 for alg in rollout bfs; do
-  for fs in 05 10 15 30; do
-    for b in inf 1.0 2.0 4.0 8.0 16.0; do
+  for fs in 05 15; do
+    for b in inf 2.0 4.0 16.0; do
       for f in 0 1 2 3; do
         ./generate_single_parameter_files.sh $alg $fs $b $f 0 "--execute-single-action,--nodisplay" $roms_path $roms_path/README.uniq > parameters/alg=$alg.fs=$fs.budget=$b.features=$f.ns=0.$suffix.txt;
         ./generate_single_parameter_files.sh $alg $fs $b $f 1 "--execute-single-action,--nodisplay,--novelty-subtables" $roms_path $roms_path/README.uniq > parameters/alg=$alg.fs=$fs.budget=$b.features=$f.ns=1.$suffix.txt;

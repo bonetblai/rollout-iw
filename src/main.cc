afddaca9debb9f8a7478ede318f2f941d66de475
Blai Bonet <bonetblai@gmail.com>
2017-11-12 18:55:40 -0400

    set default discount to 1.0

--- src/main.cc
+++ src/main.cc
@@ -306,7 +306,7 @@ int main(int argc, char **argv) {
       ("novelty-subtables", "Turn on use of novelty subtables (default is to use single table)")
       ("random-actions", "Use random action when there are no rewards in look-ahead tree (default is off)")
       ("max-rep", po::value<int>(&opt_max_rep)->default_value(30), "Set max rep(etition) of screen features during lookahead (default is 30)")
-      ("discount", po::value<float>(&opt_discount)->default_value(0.95), "Set discount factor for lookahead (default is 0.95)")
+      ("discount", po::value<float>(&opt_discount)->default_value(1.00), "Set discount factor for lookahead (default is 1.00)")
       ("alpha", po::value<float>(&opt_alpha)->default_value(50000.0), "Set alpha value for lookahead (default is 50k)")
       ("use-alpha-to-update-reward-for-death", "Assign a big negative reward, depending on alpha's value, for deaths (default is off)")
       ("nodes-threshold", po::value<int>(&opt_nodes_threshold)->default_value(50000), "Set threshold in #nodes for expanding look-ahead tree (default is 50k)")

f97eca16a0f14705d441be5e6817579c207e5563
Blai Bonet <bonetblai@gmail.com>
2017-12-10 15:47:45 +0000

    Add experiments scripts for 11n

---
+++ sge-scripts/experiment11n.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+algorithm=$1
+frameskip=$2
+time_budget=$3
+features=$4
+novelty_subtables=$5
+discount=$6
+raw_options=$7
+rom=$8
+log_file=$9
+
+export IW_ROOT=$HOME/software/github/rollout-iw
+export ALE_PATH=$HOME/software/github/Arcade-Learning-Environment
+
+ulimit -c 0
+
+options=${raw_options//,/ }
+
+$IW_ROOT/src/rom_planner \
+  --seed $RANDOM \
+  --lookahead-caching 1 \
+  --random-actions \
+  --alpha 1 \
+  --discount $discount \
+  --break-ties-using-rewards \
+  --planner $algorithm \
+  --frameskip $frameskip \
+  --time-budget $time_budget \
+  --features $features \
+  --log-file $log_file \
+  --rom $IW_ROOT/atari-roms/$rom $options
+
